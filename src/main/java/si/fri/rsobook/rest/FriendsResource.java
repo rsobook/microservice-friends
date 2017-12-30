dbab676f1896604b6029e236b3f01af620a2f0e7
Targma <jg1724@student.uni-lj.si>
2017-12-30 18:15:48 +0100

    Added fault tolerance, need fix.

--- src/main/java/si/fri/rsobook/rest/FriendsResource.java
+++ src/main/java/si/fri/rsobook/rest/FriendsResource.java
@@ -1,24 +1,16 @@
 package si.fri.rsobook.rest;
 
-import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import com.kumuluz.ee.logs.cdi.Log;
 import org.eclipse.microprofile.metrics.Counter;
 import org.eclipse.microprofile.metrics.annotation.Metric;
-import si.fri.rsobook.config.FriendsConfigProperties;
-import si.fri.rsobook.core.api.ApiConfiguration;
-import si.fri.rsobook.core.api.ApiCore;
-import si.fri.rsobook.core.api.client.utility.QueryParamBuilder;
-import si.fri.rsobook.core.api.data.response.PagingResponse;
 import si.fri.rsobook.core.api.exception.ApiException;
-import si.fri.rsobook.core.api.resource.base.CrudApiResource;
 import si.fri.rsobook.core.database.dto.AuthEntity;
-import si.fri.rsobook.core.database.dto.Paging;
-import si.fri.rsobook.core.database.exceptions.BusinessLogicTransactionException;
 import si.fri.rsobook.core.database.impl.DatabaseImpl;
 import si.fri.rsobook.core.model.User;
 import si.fri.rsobook.core.model.UserFriends;
 import si.fri.rsobook.core.restComponenets.resource.CrudResource;
 import si.fri.rsobook.service.DatabaseService;
+import si.fri.rsobook.service.FriendsBean;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -26,8 +18,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,14 +26,10 @@ import java.util.UUID;
 public class FriendsResource extends CrudResource<UUID, UserFriends> {
 
     @Inject
-    @DiscoverService(value = "ms-user", version = "2.0.x", environment = "dev")
-    private URL url;
-
-    @Inject
-    private FriendsConfigProperties friendsConfigProperties;
-
-    @Inject
     private DatabaseService databaseService;
+
+    @Inject
+    private FriendsBean friends;
 
     @Inject
     @Metric(name = "friends_resolved")
@@ -58,9 +44,7 @@ public class FriendsResource extends CrudResource<UUID, UserFriends> {
     @Path("resolve/{id}")
     public Response getUserFriendsResolved(@PathParam("id") UUID id) throws ApiException {
 
-        List<UUID> ids = getFriendsUUIDs(id);
-        List<User> resolvedFriends = getResolvedList(ids);
-
+        List<User> resolvedFriends = friends.getFriends(id);
         friendsResolvedCounter.inc(resolvedFriends.size());
 
         return Response.ok(resolvedFriends).build();
@@ -81,57 +65,4 @@ public class FriendsResource extends CrudResource<UUID, UserFriends> {
         return databaseService;
     }
 
-    private List<UUID> getFriendsUUIDs(UUID id) throws ApiException {
-
-        final UUID qId = id;
-        try {
-            Paging<UserFriends> friendsList = databaseService.getList(UserFriends.class, (p, cb, r) -> cb.equal(r.get("userId"), qId));
-
-            List<UUID> ids = new ArrayList<>();
-            for(UserFriends uf : friendsList.getItems()) {
-                ids.add(uf.getFriendsId());
-            }
-
-            return ids;
-        } catch (BusinessLogicTransactionException e) {
-            e.printStackTrace();
-            throw new ApiException("Error", e);
-        }
-    }
-
-    private List<User> getResolvedList(List<UUID> ids) throws ApiException {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        for(UUID id : ids){
-            sb.append(id.toString());
-            sb.append(",");
-        }
-        sb.append("]");
-
-        QueryParamBuilder queryParamBuilder = new QueryParamBuilder();
-        queryParamBuilder.addCond("id:in:" + sb.toString());
-
-        String host = friendsConfigProperties.getUserApiHost();
-        if(friendsConfigProperties.getUserApiHostDiscovery() && url != null) {
-            host = url.toString();
-        }
-
-        ApiConfiguration config = new ApiConfiguration(String.format(
-                "%s/api/v1", host));
-
-        ApiCore apiCore = new ApiCore(config, null);
-        CrudApiResource<User> resource = new CrudApiResource<>(apiCore, User.class);
-
-        try {
-            String query = queryParamBuilder.buildQuery();
-            PagingResponse<User> response = resource.get(query);
-            if(response.isStatusValid()) {
-                return response.getItems();
-            }
-        } catch (ApiException e) {
-            e.printStackTrace();
-        }
-        throw new ApiException("Error processing users.");
-    }
 }
---
+++ src/main/java/si/fri/rsobook/service/FriendsBean.java
@@ -0,0 +1,118 @@
+package si.fri.rsobook.service;
+
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import si.fri.rsobook.config.FriendsConfigProperties;
+import si.fri.rsobook.core.api.ApiConfiguration;
+import si.fri.rsobook.core.api.ApiCore;
+import si.fri.rsobook.core.api.client.utility.QueryParamBuilder;
+import si.fri.rsobook.core.api.data.response.PagingResponse;
+import si.fri.rsobook.core.api.resource.base.CrudApiResource;
+import si.fri.rsobook.core.database.dto.Paging;
+import si.fri.rsobook.core.database.exceptions.BusinessLogicTransactionException;
+import si.fri.rsobook.core.model.User;
+import si.fri.rsobook.core.model.UserFriends;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.InternalServerErrorException;
+import java.net.URL;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequestScoped
+public class FriendsBean {
+
+    @Inject
+    private FriendsConfigProperties friendsConfigProperties;
+
+    @Inject
+    private DatabaseService databaseService;
+
+    @Inject
+    @DiscoverService(value = "ms-user", version = "2.0.x", environment = "dev")
+    private URL url;
+
+    public List<User> getFriends(UUID id) {
+        List<UUID> ids = getFriendsUUIDs(id);
+        return getResolvedList(ids);
+    }
+
+    public List<UUID> getFriendsUUIDs(UUID id) {
+
+        final UUID qId = id;
+        try {
+            Paging<UserFriends> friendsList = databaseService.getList(UserFriends.class, (p, cb, r) -> cb.equal(r.get("userId"), qId));
+
+            List<UUID> ids = new ArrayList<>();
+            for(UserFriends uf : friendsList.getItems()) {
+                ids.add(uf.getFriendsId());
+            }
+
+            return ids;
+        } catch (BusinessLogicTransactionException e) {
+            e.printStackTrace();
+            throw new InternalServerErrorException("Error", e);
+        }
+    }
+
+    @CircuitBreaker(requestVolumeThreshold = 1)
+    @Fallback(fallbackMethod = "getResolvedListFallback")
+    @Timeout(value = 8, unit = ChronoUnit.SECONDS)
+    public List<User> getResolvedList(List<UUID> ids) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for(UUID id : ids){
+            sb.append(id.toString());
+            sb.append(",");
+        }
+        sb.append("]");
+
+        QueryParamBuilder queryParamBuilder = new QueryParamBuilder();
+        queryParamBuilder.addCond("id:in:" + sb.toString());
+
+        String host = friendsConfigProperties.getUserApiHost();
+        if(friendsConfigProperties.getUserApiHostDiscovery() && url != null) {
+            host = url.toString();
+        }
+
+        ApiConfiguration config = new ApiConfiguration(String.format(
+                "%s/api/v1", host));
+
+        ApiCore apiCore = new ApiCore(config, null);
+        CrudApiResource<User> resource = new CrudApiResource<>(apiCore, User.class);
+
+        try {
+            String query = queryParamBuilder.buildQuery();
+            PagingResponse<User> response = resource.get(query);
+            if(response.isStatusValid()) {
+                return response.getItems();
+            } else {
+                throw new InternalServerErrorException("User service is not responding.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new InternalServerErrorException("Error processing users.", e);
+        }
+    }
+
+    public List<User> getResolvedListFallback(List<UUID> ids) {
+
+        User user = new User();
+
+        user.setName("Chuck");
+        user.setSurname("Norris");
+        user.setEmail("chuck@norris.com");
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+
+        return userList;
+    }
+
+}
